bf1ec7e9f0153a8e45fdfc30e2e4da0cdcb6b888
Knoppix User <knoppix@Microknoppix.(none)>
2015-01-12 10:48:19 +0100

    primo commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double k,i,j;
+	float dim;
+	char messaggio[dim];
+	char tabella[k][j];
+	char lettera;
+	cin>>dim;
+	for (i=0;i<dim;i++)
+	{
+		cin>>lettera;
+		lettera=messaggio[i];
+	}
+	for (i=0;i<dim;i++)
+	     cout<<messaggio[i];
+	return 0;
+}
